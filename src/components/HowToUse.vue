1e00889ddadaf20ace34df8b5a4dee325d5cd763
TheNitpickyCloud <67553169+TheNitpickyCloud@users.noreply.github.com>
2022-05-22 12:18:47 +0530

    commit-17
    improved how to use

--- src/components/HowToUse.vue
+++ src/components/HowToUse.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="info">
-    control/command click on a node, then control/command click again on another/same node to add an edge between the two nodes.
+    <h3>To add an edge between two nodes:</h3>
+    <ol>
+    <li><kbd>Ctrl</kbd>/<kbd>Cmd</kbd> + click on the source node</li>
+    <br>
+    <li><kbd>Ctrl</kbd>/<kbd>Cmd</kbd> + click on destination node</li>
+    </ol>
+    <p><small>note: source and destination nodes can be the same</small></p>
   </div>
 </template>
 
@@ -12,8 +18,35 @@ export default {
 
 <style>
 .info{
-    padding: 5px;
-    margin-left: 10px;
-    margin-right: 10px;
+  padding: 5px;
+  margin-left: 1px;
+  margin-right: 1px;
+  font-size: 0.8em;
+}
+kbd {
+  --kbd-color-background: hsl(252, 10%, 85%);
+  --kbd-color-border:     hsl(252, 10%, 75%);
+  --kbd-color-text:       hsl(252, 10%, 25%);
+  background-color: var(--kbd-color-background);
+  border-radius: 0.3em;
+  border: 0.1em solid var(--kbd-color-border);
+  box-shadow: 0 0.1em 0 0.1em var(--kbd-color-border);
+  color: var(--kbd-color-text);
+  cursor: default;
+  display: inline-block;
+  font-family: sans-serif;
+  font-weight:bold;
+  font-size: 0.875em;
+  line-height: 1;
+  min-width: 1rem;
+  padding:0.1em 0.4em;
+  margin:0 0.2em;
+  position: relative;
+  text-align: center;
+  top: -1px;
+}
+kbd:hover {
+  box-shadow: 0 1px 0 0.5px var(--kbd-color-border);
+  top: 1px;
 }
 </style>
